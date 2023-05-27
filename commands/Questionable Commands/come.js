c9687db79bc66c3034d68b2a5036bf5737de80ce
Jasjit <jasjitbansia@gmail.com>
2023-05-27 16:53:34 +0530

    Enlargement of text added in 2 commands

--- commands/Questionable Commands/come.js
+++ commands/Questionable Commands/come.js
@@ -15,20 +15,37 @@ module.exports = {
         type: 4,
         required: true,
       },
+      {
+        name: "enlarge",
+        description: "Enlarge the text",
+        type: 5,
+        required: false,
+      },
     ],
   },
   execute(interaction) {
-    if (interaction.commandName === "come") {
-      let user = `<@${interaction.options.data[0].user.id}>`;
-      let numberOfPings = interaction.options.data[1].value - 1;
-      if (numberOfPings < 20) {
-        interaction.reply(`Cum here ${user}`);
-        for (let i = 1; i <= numberOfPings; i++) {
-          interaction.channel.send(`Cum here ${user}`);
+    let user = `<@${interaction.options.data[0].user.id}>`;
+    let numberOfPings = interaction.options.data[1].value;
+
+    let sendText = (parameter) => {
+      interaction.channel.send(`${parameter} Cum here ${user}`);
+    };
+
+    if (numberOfPings <= 20) {
+      interaction.reply(`Pinging...`);
+
+      for (let i = 1; i <= numberOfPings; i++) {
+        if (
+          interaction.options.data[2] &&
+          interaction.options.data[2].value === true
+        ) {
+          sendText("#");
+        } else {
+          sendText("");
         }
-      } else {
-        interaction.reply("The limit of pings is 20.");
       }
+    } else {
+      interaction.reply("The limit of pings is 20.");
     }
   },
 };
