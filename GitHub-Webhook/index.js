ef3213999588d2e3a328fe8762af6e690ccd1fda
Jasjit Singh <jasjitbansia@gmail.com>
2025-02-28 16:54:17 +0530

    Merged webhook repo with bot

---
+++ GitHub-Webhook/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const tokens = require("../tokens.json");
+const port = 4500;
+const {
+  Client,
+  WebhookClient,
+  EmbedBuilder,
+  GatewayIntentBits,
+} = require("discord.js");
+const app = express();
+app.use(express.json());
+const client = new Client({
+  intents: [
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+  ],
+});
+client.on("ready", () => {
+  console.log("Bot is ready");
+});
+const webhookClient = new WebhookClient({
+  url: tokens.webhookURL,
+});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/github-webhook", async (req, res) => {
+  let commitMessage;
+  let important = false;
+  let data = req.body;
+  if (data.head_commit.message.toLowerCase().endsWith("-i")) {
+    commitMessage = data.head_commit.message.substring(
+      0,
+      data.head_commit.message.length - 2
+    );
+    important = true;
+  } else {
+    commitMessage = data.head_commit.message;
+  }
+  let user = await client.users.fetch("764047181228408873");
+  const embed = new EmbedBuilder()
+    .setTitle("Makhani Parchai-New Commit")
+    .setColor("Green")
+    .setThumbnail(user.displayAvatarURL())
+    .setDescription(commitMessage + " -" + data.head_commit.committer.username);
+
+  webhookClient.send({
+    content: important ? "@everyone" : "Minor Commit",
+    username: "Makhani-Parchai",
+    avatarURL: user.displayAvatarURL(),
+    embeds: [embed],
+  });
+  res.status(200);
+});
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
+client.login(tokens.webhookBot);
